8152386011103e2ffa7bb415ed35865da9ff2a80
Tymur K <113860350+trlumph@users.noreply.github.com>
2024-08-03 15:48:57 +0300

    Create yolo_model.py

---
+++ yolo_model.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, field
+from typing import List, ClassVar
+
+@dataclass
+class YOLOModelInfo:
+    classes: List[str] = field(default_factory=lambda: [
+        'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat',
+        'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
+        'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella',
+        'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite',
+        'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle',
+        'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+        'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+        'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+        'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book',
+        'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+    ])
+    image_width: int = 640
+    image_height: int = 640
+    color_palette: List[List[int]] = field(init=False)
+
+    def __post_init__(self):
+        import numpy as np
+        self.color_palette = np.random.uniform(0, 255, size=(len(self.classes), 3)).tolist()
